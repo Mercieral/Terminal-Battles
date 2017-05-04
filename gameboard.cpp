8c98a798799cec6871a66b3474ce0dd96eacab0b
skywalker95 <lmdani95@gmail.com>
2017-05-04 12:51:18 -0400

    Initialized Game Pieces and Pulled In Master Update

--- gameboard.cpp
+++ gameboard.cpp
@@ -1,6 +1,8 @@
 #include "gameboard.hpp"
 #include "game_piece.hpp"
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -23,7 +25,16 @@ void Gameboard::initializeBoard () {
 }
 
 void Gameboard::initializeGamePieces() {
-
+  Game_Piece aircraft_carrier = Game_Piece(5, 'a');
+  piece_array[0] = aircraft_carrier;
+  Game_Piece battleship = Game_Piece(4, 'b');
+  piece_array[1] = battleship;
+  Game_Piece destroyer = Game_Piece(3, 'd');
+  piece_array[2] = destroyer;
+  Game_Piece submarine = Game_Piece(3, 's');
+  piece_array[3] = submarine;
+  Game_Piece patrol_boat = Game_Piece(2, 'p');
+  piece_array[4] = patrol_boat;
 }
 
 void Gameboard::printBoard() {
@@ -38,5 +49,17 @@ void Gameboard::printBoard() {
 }
 
 void Gameboard::generateBoardPlacement() {
-  cout << "Hi There\n";
+  for (int i = 0; i < 5; i++) {
+    cout << "This is the piece: " << piece_array[i].Get_Piece_Symbol() << '\n';
+    int starting_peg = rand() % 100 + 1;
+    cout << "Random Start: " << to_string(starting_peg) << '\n';
+    int orientation = rand() % 2 + 1;
+    cout << "Orientation: " << to_string(orientation) << '\n';
+    if (orientation == 1) {
+      //+- 10
+    } //Vertical
+    else {
+      //+- 1
+    } //Horizontal
+  }
 }
