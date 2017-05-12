90a91e49c5906e96796492c1c7a8a544b060da6a
Aaron Mercier <mercieal@rose-hulman.edu>
2017-05-12 16:04:13 -0500

    menu instead of using args on startup

--- main.cpp
+++ main.cpp
@@ -3,56 +3,159 @@
 
 using namespace std;
 
-void signal_handler(int sig) {
-    endwin();
-    exit(0);
+void setupWindow();
+char startMenu();
+void signal_handler(int sig)
+{
+	endwin();
+	exit(0);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+	setupWindow();
+	char ret = 'e';
+	while (ret != 'h' && ret != 'c')
+	{
+		for (int i = 8; i < 30; i++)
+		{
+			mvprintw(i, 0, "\n");
+		}
+		refresh();
+		ret = startMenu();
+		if (ret == 'h')
+		{
+			Host host = Host();
+			for (int i = 8; i < 30; i++)
+			{
+				mvprintw(i, 0, "\n");
+			}
+			refresh();
+			host.connect();
+		}
+		else if (ret == 'c')
+		{
+			for (int i = 8; i < 30; i++)
+			{
+				mvprintw(i, 0, "\n");
+			}
+			mvprintw(8, 2, "Enter the hostname or IP of the host: ");
+			char *str = (char *)malloc(100);
+			echo();
+			getnstr(str, 100);
+			noecho();
+			if (strcmp(str, "") == 0)
+			{
+				ret = 'e';
+			}
+			else
+			{
+				Client client = Client(str);
+				refresh();
+				client.connect();
+			}
+		}
+	}
 
-    printf("\e[8;30;101t");
-    initscr();
-    start_color();
+	endwin();
+	return 0;
+}
 
-    init_color(COLOR_CYAN, 250,250,250);
-    init_color(COLOR_BLACK, 0, 0, 0);
+void setupWindow()
+{
+	printf("\e[8;30;101t");
+	initscr();
+	start_color();
 
-    init_pair(1, COLOR_WHITE, COLOR_BLACK);
-    init_pair(2, COLOR_WHITE, COLOR_CYAN);
-    init_pair(3, COLOR_WHITE, COLOR_BLUE);
-    init_pair(4, COLOR_BLACK, COLOR_RED);
-    resizeterm(30, 101);
-    noecho();
-    refresh();
-    attron(COLOR_PAIR(1));
-    keypad(stdscr, TRUE);
-    printw(
-            " ______     ______     ______   ______   __         ______     ______     __  __     __     ______  \n"
-                    "/\\  == \\   /\\  __ \\   /\\__  _\\ /\\__  _\\ /\\ \\       /\\  ___\\   /\\  ___\\   /\\ \\_\\ \\   /\\ \\   /\\  == \\ \n"
-                    "\\ \\  __<   \\ \\  __ \\  \\/_/\\ \\/ \\/_/\\ \\/ \\ \\ \\____  \\ \\  __\\   \\ \\___  \\  \\ \\  __ \\  \\ \\ \\  \\ \\  _-/ \n"
-                    " \\ \\_____\\  \\ \\_\\ \\_\\    \\ \\_\\    \\ \\_\\  \\ \\_____\\  \\ \\_____\\  \\/\\_____\\  \\ \\_\\ \\_\\  \\ \\_\\  \\ \\_\\   \n"
-                    "  \\/_____/   \\/_/\\/_/     \\/_/     \\/_/   \\/_____/   \\/_____/   \\/_____/   \\/_/\\/_/   \\/_/   \\/_/   \n"
-                    "                                                                                                    \n",
-            "Hi");
-    refresh();
+	init_color(COLOR_CYAN, 250, 250, 250);
+	init_color(COLOR_BLACK, 0, 0, 0);
 
-    // catch ctrl-c and close sockets
-    signal(SIGINT, signal_handler);
+	init_pair(1, COLOR_WHITE, COLOR_BLACK);
+	init_pair(2, COLOR_WHITE, COLOR_CYAN);
+	init_pair(3, COLOR_WHITE, COLOR_BLUE);
+	init_pair(4, COLOR_BLACK, COLOR_RED);
+	resizeterm(30, 101);
+	noecho();
+	refresh();
+	attron(COLOR_PAIR(1));
+	keypad(stdscr, TRUE);
+	printw(
+		" ______     ______     ______   ______   __         ______     ______     __  __     __     ______  \n"
+		"/\\  == \\   /\\  __ \\   /\\__  _\\ /\\__  _\\ /\\ \\       /\\  ___\\   /\\  ___\\   /\\ \\_\\ \\   /\\ \\   /\\  == \\ \n"
+		"\\ \\  __<   \\ \\  __ \\  \\/_/\\ \\/ \\/_/\\ \\/ \\ \\ \\____  \\ \\  __\\   \\ \\___  \\  \\ \\  __ \\  \\ \\ \\  \\ \\  _-/ \n"
+		" \\ \\_____\\  \\ \\_\\ \\_\\    \\ \\_\\    \\ \\_\\  \\ \\_____\\  \\ \\_____\\  \\/\\_____\\  \\ \\_\\ \\_\\  \\ \\_\\  \\ \\_\\   \n"
+		"  \\/_____/   \\/_/\\/_/     \\/_/     \\/_/   \\/_____/   \\/_____/   \\/_____/   \\/_/\\/_/   \\/_/   \\/_/   \n"
+		"                                                                                                    \n");
+	refresh();
 
-    if (argc == 2) {
-        printw("You are a client connecting to ");
-        printw(argv[1]);
-        printw("\n");
-        refresh();
-        Client client = Client(argv[1]);
-        client.connect();
-    } else {
-        printw("You are a host\n");
-        refresh();
-        Host host = Host();
-        host.connect();
-    }
+	// catch ctrl-c and close sockets
+	signal(SIGINT, signal_handler);
+}
 
-    endwin();
-    return 0;
+char startMenu()
+{
+	attron(A_UNDERLINE);
+	move(8, 2);
+	printw("How do you want to start?");
+	attroff(A_UNDERLINE);
+	string options[3] = {"- Run as a new host", "- Connect to a waiting host", "- exit"};
+	int i = 0;
+	move(10, 4);
+	for (i = 0; i < 3; i++)
+	{
+		move(10 + i, 4);
+		if (i == 0)
+			attron(A_STANDOUT); // highlights the first item.
+		else
+			attroff(A_STANDOUT);
+		printw(options[i].c_str());
+	}
+	refresh();
+	i = 0;
+	curs_set(0);
+	bool isActiveMenu = true;
+	int prev = 0;
+	char ret = 'e';
+	while (isActiveMenu)
+	{
+		prev = i;
+		// use a variable to increment or decrement the value based on the input.
+		switch (getch())
+		{
+		case KEY_UP:
+			i--;
+			i = (i < 0) ? 2 : i;
+			break;
+		case KEY_DOWN:
+			i++;
+			i = (i > 2) ? 0 : i;
+			break;
+		case 10:
+			switch (i)
+			{
+			case 0:
+				mvprintw(14, 2, "selected host\n");
+				ret = 'h';
+				isActiveMenu = false;
+				break;
+			case 1:
+				mvprintw(14, 2, "selected client\n");
+				ret = 'c';
+				isActiveMenu = false;
+				break;
+			case 2:
+				endwin(); // Finishes graphics
+				cout << "Ending game due to a player quiting\n";
+				exit(1);
+			}
+		}
+		// now highlight the next item in the list.
+		attroff(A_STANDOUT);
+		mvprintw(10 + prev, 4, options[prev].c_str());
+		attron(A_STANDOUT);
+		mvprintw(10 + i, 4, options[i].c_str());
+		attroff(A_STANDOUT);
+	}
+	curs_set(2);
+	return ret;
 }
--- user/host.cpp
+++ user/host.cpp
@@ -1,90 +1,108 @@
 #include "user.hpp"
 
-void Host::connect() {
-    int host_socket;
-    struct sockaddr_in server_address;
+void Host::connect()
+{
+	int host_socket;
+	struct sockaddr_in server_address;
 
-    //Initialize the endpoint structure
-    memset(&server_address, 0, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = htons(PORT);
-    printw("Created the endpoint structure\n");
-    refresh();
+	move(8, 0);
 
-    //Create socket
-    host_socket = socket(PF_INET, SOCK_STREAM, 0);
-    if (host_socket < 0) {
-        endwin();
-        cout << "Error creating host socket. Error = " << strerror(errno) << "\n";
-        close(host_socket);
-        exit(1);
-    }
-    printw("Created the host socket\n");
-    refresh();
+	//Initialize the endpoint structure
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = INADDR_ANY;
+	server_address.sin_port = htons(PORT);
+	printw("Created the endpoint structure\n");
+	refresh();
 
-    int on = 1;
-    if (setsockopt(host_socket, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) {
-        endwin();
-        cout << "Something went wrong when setting socket options... " << strerror(errno) << "\n";
-        exit(1);
-    }
+	//Create socket
+	host_socket = socket(PF_INET, SOCK_STREAM, 0);
+	if (host_socket < 0)
+	{
+		endwin();
+		cout << "Error creating host socket. Error = " << strerror(errno) << "\n";
+		close(host_socket);
+		exit(1);
+	}
+	printw("Created the host socket\n");
+	refresh();
 
-    //Passive Open
-    if (bind(host_socket, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
-        endwin();
-        cout << "Error trying to bind and connect socket. Error = " << strerror(errno) << "\n";
-        close(host_socket);
-        exit(1);
-    }
-    listen(host_socket, 5);
-    printw("Socket bound and listening\n");
-    refresh();
+	int on = 1;
+	if (setsockopt(host_socket, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0)
+	{
+		endwin();
+		cout << "Something went wrong when setting socket options... " << strerror(errno) << "\n";
+		exit(1);
+	}
 
-    struct sockaddr_in client_address;
-    socklen_t address_size = sizeof(client_address);
+	//Passive Open
+	if (bind(host_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+	{
+		endwin();
+		cout << "Error trying to bind and connect socket. Error = " << strerror(errno) << "\n";
+		close(host_socket);
+		exit(1);
+	}
+	listen(host_socket, 5);
+	printw("Socket bound and listening\n");
+	refresh();
 
-    while (true) {
-        if ((client_socket = accept(host_socket, (struct sockaddr *) &client_address, &address_size)) < 0) {
-            endwin();
-            cout << "Error trying to accept client connection. Error = " << strerror(errno) << "\n";
-            close(host_socket);
-            exit(1);
-        }
+	struct sockaddr_in client_address;
+	socklen_t address_size = sizeof(client_address);
 
-        if (acceptClientConnection()) {
-            printw("Connection was accepted, continuing\n");
-            refresh();
-            const char *msg = "Connection accepted by host\n";
-            send(client_socket, msg, strlen(msg), 0);
-            this->gameLoop(client_socket);
-        } else {
-            close(client_socket);
-            printw("Connection was rejected, closing client socket\n");
-            refresh();
-        }
-    }
+	while (true)
+	{
+		if ((client_socket = accept(host_socket, (struct sockaddr *)&client_address, &address_size)) < 0)
+		{
+			endwin();
+			cout << "Error trying to accept client connection. Error = " << strerror(errno) << "\n";
+			close(host_socket);
+			exit(1);
+		}
+
+		if (acceptClientConnection())
+		{
+			printw("Connection was accepted, continuing\n");
+			refresh();
+			const char *msg = "Connection accepted by host\n";
+			send(client_socket, msg, strlen(msg), 0);
+			this->gameLoop(client_socket);
+		}
+		else
+		{
+			close(client_socket);
+			printw("Connection was rejected, closing client socket\n");
+			refresh();
+		}
+	}
 }
 
 //Lets the host accept or reject an established connection with a client
-bool Host::acceptClientConnection() {
-    printw("\nWould you like to accept the connection, Y/N?\n");
-    refresh();
+bool Host::acceptClientConnection()
+{
+	printw("\nWould you like to accept the connection, Y/N?\n");
+	refresh();
 
-    while (1) {
-        char user_input;
-        user_input = getch();
-        if (user_input == 'Y' || user_input ==  'y') {
-            printw("accepting connection! Have fun!\n");
-            refresh();
-            return true;
-        } else if (user_input == 'N' || user_input == 'n') {
-            printw("Connection refused... waiting for a new connection request\n");
-            refresh();
-            return false;
-        } else {
-            printw("Invalid input, please enter Y for yes or N for no\n");
-        }
-        refresh();
-    }
+	while (1)
+	{
+		char user_input;
+		user_input = getch();
+		if (user_input == 'Y' || user_input == 'y')
+		{
+			printw("accepting connection! Have fun!\n");
+			refresh();
+			return true;
+		}
+		else if (user_input == 'N' || user_input == 'n')
+		{
+			printw("Connection refused... waiting for a new connection request\n");
+			refresh();
+			return false;
+		}
+		else
+		{
+			printw("Invalid input, please enter Y for yes or N for no\n");
+		}
+		refresh();
+	}
 }
